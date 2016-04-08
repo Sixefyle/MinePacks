a1d817f7b7f57ec3c69c18adb0526b0d3d05b0a9
Sixefyle <writesixefyle@gmail.com>
2016-04-08 11:42:09 +0200

    MinePacks

---
+++ index.php
@@ -0,0 +1,236 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>MinesPack</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  <header>
+	  <nav class="navbar navbar-default navbar-fixed-top">
+	  <div class="container-fluid">
+	    <!-- Brand and toggle get grouped for better mobile display -->
+	    <div class="navbar-header">
+	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+	        <span class="sr-only">Toggle navigation</span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	      </button>
+	      <a class="navbar-brand" href="#">MinesPack</a>
+	    </div>
+	
+	    <!-- Collect the nav links, forms, and other content for toggling -->
+	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	      <ul class="nav navbar-nav">
+	        <li class="active"><a href="#">Accueil <span class="sr-only">(current)</span></a></li>
+	        <li><a href="#">Nouveauté</a></li>
+	        <li class="dropdown">
+	          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Ressources packs <span class="caret"></span></a>
+	          <ul class="dropdown-menu">
+	            <li><a href="#">1.9</a></li>
+	            <li><a href="#">PVP</a></li>
+	            <li><a href="#">Réaliste</a></li>
+	            <li><a href="#">Médieval</a></li>
+	            <li><a href="#">Autres</a></li>
+	            <li role="separator" class="divider"></li>
+	            <li><a href="#">1.8</a></li>
+	            <li><a href="#">PVP</a></li>
+	            <li><a href="#">Réaliste</a></li>
+	            <li><a href="#">Médieval</a></li>
+	            <li><a href="#">Autres</a></li>
+	            <li role="separator" class="divider"></li>
+	            <li><a href="#">1.7</a></li>
+	            <li><a href="#">PVP</a></li>
+	            <li><a href="#">Réaliste</a></li>
+	            <li><a href="#">Médieval</a></li>
+	            <li><a href="#">Autres</a></li>
+	          </ul>
+	        </li>
+	      </ul>
+	      <form class="navbar-form navbar-left" role="search">
+	        <div class="form-group">
+	          <input type="text" class="form-control" placeholder="Ressource pack">
+	        </div>
+	        <button type="submit" class="btn btn-default">Rechercher</button>
+	      </form>
+	      <ul class="nav navbar-nav navbar-right">
+	        <li><a href="#">S'inscrire</a></li>
+	        <li><a href="#">Se connecter</a></li>
+	      </ul>
+	    </div><!-- /.navbar-collapse -->
+	  </div><!-- /.container-fluid -->
+	</nav>
+  </header>
+  <!-- Colonne de gauche -->
+	<div class="col-xs-4 col-sm-2 affix" style="background-color: #101010; height: 100vh; padding-top: 70px;">
+		  <a href="test" class="list-group-item" style="background-color: #E8E8E8;">
+		    <span class="badge">751</span>
+		    Ressource pack 1.9
+		  </a>
+		  <a href="test" class="list-group-item">
+		    <span class="badge">500</span>
+		    PVP
+		  </a>
+		  <a href="test" class="list-group-item">
+		    <span class="badge">251</span>
+		    Realiste
+		  </a>	  
+		  <a href="test" class="list-group-item">
+		    <span class="badge">251</span>
+		    Médieval
+		  </a>	  
+		  <a href="test" class="list-group-item">
+		    <span class="badge">251</span>
+		    Autres
+		  </a>	  
+		  <br />
+		  <a href="test" class="list-group-item" style="background-color: #E8E8E8;">
+		    <span class="badge">17 524</span>
+		    Ressource pack 1.8
+		  </a>
+		  <a href="test" class="list-group-item">
+		    <span class="badge">15 000</span>
+		    PVP
+		  </a>
+		  <a href="test" class="list-group-item">
+		    <span class="badge">524</span>
+		    Realiste
+		  </a>	  
+		  <a href="test" class="list-group-item">
+		    <span class="badge">1 500</span>
+		    Médieval
+		  </a>	  
+		  <a href="test" class="list-group-item">
+		    <span class="badge">500</span>
+		    Autres
+		  </a>	  
+		  <br />
+		  <a href="test" class="list-group-item" style="background-color: #E8E8E8;">
+		    <span class="badge">751</span>
+		    Ressource pack 1.7
+		  </a>
+		  <a href="test" class="list-group-item">
+		    <span class="badge">251</span>
+		    PVP
+		  </a>
+		  <a href="test" class="list-group-item">
+		    <span class="badge">200</span>
+		    Realiste
+		  </a>	  
+		  <a href="test" class="list-group-item">
+		    <span class="badge">50</span>
+		    Médieval
+		  </a>	  
+		  <a href="test" class="list-group-item">
+		    <span class="badge">150</span>
+		    Autres
+		  </a>
+		  <br />
+		<button type="button" class="btn btn-default btn-lg" style="margin-left: 6.5%;">
+		  Proposer votre pack <span class="glyphicon glyphicon-cloud-upload" aria-hidden="true"></span>
+		</button>	  
+	</div>
+  <!-- Fin de la coloone de gauche -->
+  <!-- Corp de la page -->
+	<div class="jumbotron" style="padding-left: 320px;">
+	  <h1 class="text-center">MinesPack</h1>
+	  <p class="text-center">MinesPack est un site regroupant les ressources packs de minecraft de la 1.7 à la 1.9 !</p>
+	  <p class="text-center">Inscrivez vous et proposer vos ressources packs !</p>
+	  <center>
+	  	<p><a class="btn btn-primary btn-lg" href="#" role="button">S'inscrire maintenant !</a></p>
+	  </center>
+	</div>
+	
+	<div id="corp">
+		<div class="row">
+		  <div class="col-sm-6 col-md-4">
+		    <div class="thumbnail">
+		      <img src="./images/Test1.png" alt="...">
+		      <div class="caption">
+		       <h3><span class="label label-danger">PVP</span> The danger</h3><br />
+		        <p><a href="#" class="btn btn-primary" role="button">Télécharger <span class="glyphicon glyphicon-cloud-download" aria-hidden="true"></span></a> <a href="#" class="btn btn-default" role="button">En voir plus !</a></p>
+		      </div>
+		    </div>
+		  </div>
+		  <div class="col-sm-6 col-md-4">
+		    <div class="thumbnail">
+		      <img src="./images/Test1.png" alt="...">
+		      <div class="caption">
+				<h3><span class="label label-primary">Realiste</span> Photoréaliste :o</h3><br />
+		        <p><a href="#" class="btn btn-primary" role="button">Télécharger <span class="glyphicon glyphicon-cloud-download" aria-hidden="true"></span></a> <a href="#" class="btn btn-default" role="button">En voir plus !</a></p>
+		      </div>
+		    </div>
+		  </div>
+		  <div class="col-sm-6 col-md-4">
+		    <div class="thumbnail">
+		      <img src="./images/Test1.png" alt="...">
+		      <div class="caption">
+		       <h3><span class="label label-warning">Médieval</span> The danger</h3><br />
+		        <p><a href="#" class="btn btn-primary" role="button">Télécharger <span class="glyphicon glyphicon-cloud-download" aria-hidden="true"></span></a> <a href="#" class="btn btn-default" role="button">En voir plus !</a></p>
+		      </div>
+		    </div>
+		  </div>
+		  <div class="col-sm-6 col-md-4">
+		    <div class="thumbnail">
+		      <img src="./images/Test1.png" alt="...">
+		      <div class="caption">
+		       <h3><span class="label label-default">Autres</span> The danger</h3><br />
+		        <p><a href="#" class="btn btn-primary" role="button">Télécharger <span class="glyphicon glyphicon-cloud-download" aria-hidden="true"></span></a> <a href="#" class="btn btn-default" role="button">En voir plus !</a></p>
+		      </div>
+		    </div>
+		  </div>
+		  <div class="col-sm-6 col-md-4">
+		    <div class="thumbnail">
+		      <img src="./images/Test1.png" alt="...">
+		      <div class="caption">
+		       <h3><span class="label label-danger">PVP</span> The danger</h3><br />
+		        <p><a href="#" class="btn btn-primary" role="button">Télécharger <span class="glyphicon glyphicon-cloud-download" aria-hidden="true"></span></a> <a href="#" class="btn btn-default" role="button">En voir plus !</a></p>
+		      </div>
+		    </div>
+		  </div>
+		  <div class="col-sm-6 col-md-4">
+		    <div class="thumbnail">
+		      <img src="./images/Test1.png" alt="...">
+		      <div class="caption">
+		       <h3><span class="label label-danger">PVP</span> The danger</h3><br />
+		        <p><a href="#" class="btn btn-primary" role="button">Télécharger <span class="glyphicon glyphicon-cloud-download" aria-hidden="true"></span></a> <a href="#" class="btn btn-default" role="button">En voir plus !</a></p>
+		      </div>
+		    </div>
+		  </div>
+		</div>
+	</div>
+	<!-- Fin du corp de la page -->
+	<!-- Footer -->
+	<nav class="navbar navbar-default navbar-fixed-bottom">
+	  <div class="container">
+	    <p class="text-center" style="color: white;">© 2016 MinesPack. Tout droit réservé. </p>
+	    <p class="text-center" style="color: white;">Développé par Sixefyle et Bulii.</p>
+	  </div>
+	</nav>
+	<!-- Fin du Footer -->
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+<style>
+#corp{
+padding-bottom: 60px;
+padding-left: 17%;
+}
+</style>
+</html>
